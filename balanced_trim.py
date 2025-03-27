e67faa618eab349cf5144432cae62f08831ab676
umarrashid <umarrashid@hotmail.dk>
2025-03-27 16:19:03 +0100

    added dataset trim

--- balanced_trim.py
+++ balanced_trim.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv("merged_binary_dataset.csv")
 
 # Define how much of each class to keep (e.g., 10% of each)
-sample_frac = 0.1
+sample_frac = 0.002
 
 # Group by Label and take a fraction from each
 balanced_subset = df.groupby('Label', group_keys=False).apply(lambda x: x.sample(frac=sample_frac, random_state=42))
